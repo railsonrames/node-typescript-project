01f37fcd21ca25f763f7363122de9583237cbc8e
Railson Rames <rames.dev@outlook.com>
2020-04-17 10:49:09 -0300

    Initial commit.

---
+++ src/routes/index.ts
@@ -0,0 +1,17 @@
+import { Router } from 'express';
+
+const routes = Router();
+
+routes.get('/', (request, response) =>
+  response.json({ message: 'Hello GoStack' }),
+);
+
+routes.post('/user', (request, response) => {
+  const { name, email } = request.body;
+
+  const user = { name, email };
+
+  return response.json(user);
+});
+
+export default routes;
